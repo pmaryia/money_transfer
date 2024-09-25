0f4beb15676c4ab8702f68eb39d29a6f451f3540
pmaryia <maria.biruk1@gmail.com>
2024-09-25 08:58:16 +0200

    Set up project

---
+++ accounts/constants.py
@@ -0,0 +1,7 @@
+from decimal import Decimal
+
+
+ZERO_DECIMAL = Decimal("0.00")
+
+TIN_MIN_LENGTH = 10
+TIN_MAX_LENGTH = 12
---
+++ accounts/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator, MinValueValidator, RegexValidator
+from django.db import models
+
+from accounts.constants import TIN_MAX_LENGTH, TIN_MIN_LENGTH, ZERO_DECIMAL
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+    # taxpayer identification number
+    tin = models.CharField(
+        max_length=TIN_MAX_LENGTH,
+        unique=True,
+        validators=[
+            MinLengthValidator(TIN_MIN_LENGTH),
+            RegexValidator(
+                regex=r"^\d+$",
+                message="Taxpayer Identification Number must contain only digits",
+            ),
+        ],
+    )
+    account = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=ZERO_DECIMAL,
+        validators=[MinValueValidator(ZERO_DECIMAL)],
+    )
+
+    REQUIRED_FIELDS = ["first_name", "last_name"]
